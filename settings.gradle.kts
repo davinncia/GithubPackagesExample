798138c0ac722de9ab4413ff48a79c0a1944839a
davinncia <mahelucas1@gmail.com>
2024-02-19 09:19:18 +0100

    add credentials

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,10 @@ dependencyResolutionManagement {
         mavenCentral()
         maven {
             url = uri("https://maven.pkg.github.com/davinncia/GithubPackagesExample")
-
+            credentials {
+                username = "lucas-mahe-insideapp"
+                password = "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"
+            }
         }
     }
 }
